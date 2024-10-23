a3c77bdcc033a9828ae9f589efb26f1252468357
MAIN6419 <dark9737@gmail.com>
2024-10-23 19:33:56 +0900

    Feat: 미들웨어 설정 추가 및 usersRouter 적용 - body 파싱을 위한 express.json() 미들웨어 추가 - pool 연결을 위한 poolConnection 미들웨어 추가 - Error 처리를 위한 errorHandler 미들웨어 추가 - usersRouter 적용

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import usersRouter from "./routes/users";
+import poolConnection from "./middlewares/poolConnection";
+import errorHandler from "./middlewares/errorHandler";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(express.json());
+app.use(poolConnection);
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000" }));
+app.use("/users", usersRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${process.env.BASE_URL}`);
